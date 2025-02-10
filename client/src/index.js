f7f247da9c721897b5b7572e1381758f6236f3fa
Tayphong043 <traingoan043@gmail.com>
2025-02-11 03:21:36 +0700

    Delete client directory

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-
-require("es6-promise").polyfill();
-require("isomorphic-fetch");
-
-ReactDOM.render(<App />, document.getElementById("root"));
